42ab7b22a7c6008808423c1b9fa0d5e2ca321846
sofia-papa <papa.sofia@outlook.it>
2021-08-26 15:31:42 +0100

    lista-cognomi

---
+++ js/myscript.js
@@ -0,0 +1,12 @@
+
+let cognomi = ["Bianchi","Neri" ,"Rossi","Verdi", "Gialli"]
+
+let cognome = prompt ("inserisci il tuo cognome")
+
+cognomi.push (cognome)
+cognomi.sort (); //lista ordinata alfabeticamente
+document.getElementById
+
+//ordinamento numerico
+
+console.log (cognomi)
