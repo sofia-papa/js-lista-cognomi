3e7c66df7c97fe3b9201b41eca7e00de409bbc5a
sofia-papa <papa.sofia@outlook.it>
2021-08-26 16:30:42 +0100

    start array from 1

--- js/myscript.js
+++ js/myscript.js
@@ -5,6 +5,6 @@ let cognome = prompt ("inserisci il tuo cognome")
 
 cognomi.push (cognome)
 cognomi.sort (); //lista ordinata alfabeticamente
-document.getElementById("number").innerHTML = cognomi.indexOf(cognome); //ordinamento numerico
+document.getElementById("number").innerHTML = cognomi.indexOf(cognome) + 1; //ordinamento numerico
 
 console.log (cognomi)
